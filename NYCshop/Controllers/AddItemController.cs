c692360deabf61fb0cc5ce2760c158728da60fba
nhatdang2111 <35389442+nhatdang2111@users.noreply.github.com>
2018-04-24 01:50:14 +0700

    Add files via upload

---
+++ NYCshop/Controllers/AddItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NYCshop.Controllers
+{
+    public class AddItemController : Controller
+    {
+        // GET: AddItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
