72f387bfd0c11282e09e50c8fe76e2517811a9d1
KhangLe <khangjig.812@gmail.com>
2018-05-04 11:13:18 +0700

    Cập nhật ngày 4-5-2018

--- NYCshop/Controllers/UserController.cs
+++ NYCshop/Controllers/UserController.cs
@@ -27,103 +27,50 @@ namespace NYCshop.Controllers
         }
 
         //
-        // GET: /User/MyPost/
-        public ActionResult MyPost()
+        // GET: /User/ChangePassword
+        public ActionResult ChangePassword()
         {
-
-
-            return View();
-        }
-
-        //
-        // GET: /User/NewPost/
-        public ActionResult NewPost()
-        {
-            List<SelectListItem> categories = listAndDict.GetListCategories();
-            categories[0].Selected = true;
-
-            ViewBag.Categories = categories;
-            ViewBag.DictCategories = listAndDict.GetDictCategories();
-
-            return View();
-        }
-
-        //
-        // POST: /User/NewPost/
-        [HttpPost]
-        public ActionResult NewPost(Product model, List<HttpPostedFileBase> files)
-        {
+            ChangePasswordViewModel model = new ChangePasswordViewModel();
             if(Session["Username"] != null)
+                model.Username = Session["Username"].ToString();
+
+            return View(model);
+        }
+
+        //
+        // POST: /User/ChangePassword
+        [HttpPost]
+        public ActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
             {
-                string username = Session["Username"] as string;
+                var user = db.Users.FirstOrDefault(u => u.Username == model.Username);
 
-                // thêm danh sách các loại sp để hiển thị lại
-                List<SelectListItem> categories = listAndDict.GetListCategories();
-                categories[0].Selected = true;
-
-                ViewBag.Categories = categories;
-                ViewBag.DictCategories = listAndDict.GetDictCategories();
-
-                if (ModelState.IsValid)
+                if (user != null)
                 {
-                    if (files != null && files[0] != null)
+                    // 1.4 Mật khẩu cũ không đúng
+                    if (md5.GetMd5Hash(model.OldPassword) != user.Password)
+                        ModelState.AddModelError("", "Mật khẩu cũ không đúng");
+                    else
                     {
-                        // lấy productID lớn nhất
-                        var productMaxID = (from p in db.Products
-                                            select p).OrderByDescending(a => a.ProductID).FirstOrDefault().ProductID;
-
-                        // thêm sp vào CSDL
-                        model.SaleStatus = false;
-                        model.Username = username;
-                        model.ProductID = productMaxID + 1;
-
-                        db.Products.Add(model);
-
-                        // copy hình ảnh vào thư mục /Images/Products và đổi tên thích hợp
-                        #region copy hình ảnh vào thư mục /Images/Products và đổi tên thích hợp
-                        string path = Server.MapPath("~/Images/Products");
-                        if (!Directory.Exists(path))
+                        string newPassword = md5.GetMd5Hash(model.NewPassword);
+                        // 1.1 trường hợp trùng mật khẩu mới và mật khẩu cũ
+                        if (newPassword == user.Password)
+                            ModelState.AddModelError("", "Mật khẩu mới không được trùng mật khẩu cũ");
+                        else
                         {
-                            Directory.CreateDirectory(path);
+                            // 2. Đổi mật khẩu thành công
+                            user.Password = md5.GetMd5Hash(model.NewPassword);
+
+                            db.SaveChanges();
+                            ViewBag.ChangePasswordMsg = "Đổi mật khẩu thành công";
                         }
-
-                        // lấy id lớn nhất trong ImagesUrl
-                        int imageMaxID = (from i in db.ImageUrls
-                                          select i).OrderByDescending(a => a.ImageID).FirstOrDefault().ImageID;
-
-                        int currentMaxID = imageMaxID + 1;
-
-                        List<ImageUrl> images = new List<ImageUrl>();
-                        for (int i = 0; i < files.Count; i++ )
-                        {
-                            HttpPostedFileBase postedFile = files[i];
-                            if (files != null)
-                            {
-                                string extension = Path.GetExtension(postedFile.FileName);
-                                string fileName = model.ProductID + "_" + (i + 1).ToString() + extension;
-                                postedFile.SaveAs(Path.Combine(path, fileName));
-
-                                // thêm đường dẫn hình ảnh vào CSDL
-                                ImageUrl imageUrl = new ImageUrl();
-                                imageUrl.ImageID = currentMaxID;
-                                imageUrl.ProductID = model.ProductID;
-                                imageUrl.Url = "/Images/Products/" + fileName;
-                                images.Add(imageUrl);
-
-                                currentMaxID++;
-                            }
-                        }
-                        #endregion
-
-                        db.ImageUrls.AddRange(images);
-                        db.SaveChanges();
-                        ViewBag.AddProductMsg = "Thêm sản phẩm thành công";
                     }
-                    else ModelState.AddModelError("", "Bạn phải chọn ảnh cho sản phẩm");
                 }
+                else ModelState.AddModelError("", "Đổi mật khẩu thất bại");
             }
 
-            return View();
+            return View(model);
         }
 	}
 }
