629f7bb365bb6a8d17080e5bffe7042ffd6114a6
kenguyenduc <32872158+kenguyenduc@users.noreply.github.com>
2018-05-07 14:39:14 +0700

    Revert "update 7-5-2018"
    This reverts commit 9bac5eea85fc5a50beb975835bda32d602f17a07.

---
+++ NYCshop/Areas/User/Controllers/AddItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NYCshop.Areas.User.Controllers
+{
+    public class AddItemController : Controller
+    {
+        // GET: User/AddItem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NYCshop/Controllers/ProductController.cs
+++ NYCshop/Controllers/ProductController.cs
@@ -11,44 +11,10 @@ namespace NYCshop.Controllers
     public class ProductController : Controller
     {
         private ExLoverShopDb db = new ExLoverShopDb();
+<<<<<<< HEAD
 
-        //
-        // GET: /Product/
-        public ActionResult Index(int categoryID, int? page)
-        {
-            List<CategoryDetailViewModel> model = new List<CategoryDetailViewModel>();
-
-            List<string> imageUrl = new List<string>();
-            // lấy danh sách các sản phẩm
-            var categoryDetail = (from c in db.Categories
-                                  from sc in db.SubCategories
-                                  from p in db.Products
-                                  where categoryID == c.CategoryID && sc.SubCategoryID == p.SubCategoryID && sc.CategoryID == c.CategoryID
-                                  select new CategoryDetailViewModel
-                                  {
-                                      ListImages = imageUrl,
-                                      Price = p.Price,
-                                      ProductID = p.ProductID,
-                                      ProductName = p.ProductName
-                                  }).ToList();
-
-            // thiết lập các ảnh của sản phẩm
-            foreach (CategoryDetailViewModel product in categoryDetail)
-            {
-                var images = (from i in db.ImageUrls
-                              where i.ProductID == product.ProductID
-                              select i.Url).ToList();
-
-                product.ListImages = images;
-            }
-
-            model = categoryDetail;
-
-            ViewBag.CategoryID = categoryID;
-
-            int pageSize = 10; // số lượng item trên 1 trang
-            int pageNumber = (page ?? 1); // trả về giá trị của page nếu nó là 1 giá trị khác null, nếu không trả về 1
-            return View(model.OrderByDescending(m => m.ProductID).ToPagedList(pageNumber, pageSize));
-        }
+        
+=======
+>>>>>>> 72f387bfd0c11282e09e50c8fe76e2517811a9d1
 	}
 }
